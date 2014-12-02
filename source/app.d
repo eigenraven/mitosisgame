36e0f493c59309abac1e3f37c222ae3282ccb404
kubasz <kubasz@localhost.localdomain>
2014-12-02 20:49:44 +0100

    WIP

--- source/app.d
+++ source/app.d
@@ -1,6 +1,44 @@
-import std.stdio;
+module app;
+import std.stdio, std.string;
+import derelict.sfml2.system;
+import derelict.sfml2.window;
+import derelict.sfml2.audio;
+import derelict.sfml2.graphics;
+import derelict.opengl3.gl3; // Opengl
+
+public enum WindowWidth = 800;
+public enum WindowHeight = 600;
+public enum WindowTitle = "Mitosis - Cellionaires";
+public sfRenderWindow* rwin;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	DerelictGL3.load();
+	DerelictSFML2System.load();
+	DerelictSFML2Window.load();
+	DerelictSFML2Audio.load();
+	DerelictSFML2Graphics.load();
+	sfVideoMode vm = sfVideoMode_getDesktopMode();
+	vm.width=WindowWidth;
+	vm.height=WindowHeight;
+	rwin = sfRenderWindow_create(vm,toStringz(WindowTitle),sfTitlebar|sfClose,null);
+	sfRenderWindow_setFramerateLimit(rwin,120);
+	sfRenderWindow_setVerticalSyncEnabled(rwin,sfTrue);
+	while(sfRenderWindow_isOpen(rwin))
+	{
+		sfRenderWindow_clear(rwin,sfColor(0,0,0,255));
+		sfEvent* ev = new sfEvent();
+		while(sfRenderWindow_pollEvent(rwin,ev))
+		{
+			switch(ev.type)
+			{
+				case sfEvtClosed:
+					sfRenderWindow_close(rwin);break;
+				default:break;
+			}
+		}
+		sfRenderWindow_display(rwin);
+	}
+	sfRenderWindow_destroy(rwin);
+	return;
 }
